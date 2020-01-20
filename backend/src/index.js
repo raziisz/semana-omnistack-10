9814ff6da79d1937c613983b50d96609895e8628
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-01-20 13:33:09 -0400

    Finalizado app web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://razi:razi@omnistack9-4tf11.mongodb.net/week10?re
   useUnifiedTopology: true 
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
